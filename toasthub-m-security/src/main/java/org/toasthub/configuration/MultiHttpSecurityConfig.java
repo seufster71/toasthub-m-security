0fa2d0fd87bd4a1018066b07817ac04c984d5253
seufster <seufster@gmail.com>
2017-08-23 19:17:17 -0400

    toasthub-m-security

--- toasthub-m-security/src/main/java/org/toasthub/configuration/MultiHttpSecurityConfig.java
+++ toasthub-m-security/src/main/java/org/toasthub/configuration/MultiHttpSecurityConfig.java
@@ -14,8 +14,7 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.toasthub.core.general.filter.TenantInterceptor;
 import org.toasthub.security.filter.ToasthubLoginFilter;
-import org.toasthub.security.service.UserManagerSvc;
-
+import org.toasthub.security.userManager.UserManagerSvc;
 import org.toasthub.controller.RestAuthenticationEntryPoint;
 
 
